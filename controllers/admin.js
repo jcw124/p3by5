dacf847485b0d7a2b58a76e67f73b07dc2c905ac
n0rthf4ce <eric.liu85@gmail.com>
2018-07-27 22:16:22 -0700

    create routes and related controllers

---
+++ controllers/admin.js
@@ -0,0 +1,13 @@
+const db = require('../models');
+
+exports.getAdmin = function (req, res) {
+    
+}
+
+exports.saveAdmin = function (req, res) {
+    
+}
+
+exports.deleteAdmin = function (req, res) {
+    
+}
---
+++ controllers/user.js
@@ -0,0 +1,13 @@
+const db = require('../models');
+
+exports.getUser = function (req, res) {
+    
+}
+
+exports.saveUser = function (req, res) {
+    
+}
+
+exports.deleteUser = function (req, res) {
+    
+}
