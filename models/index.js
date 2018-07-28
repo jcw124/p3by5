65f6bd107a8d367a951f6f2bd2f85227fd10cdf2
n0rthf4ce <eric.liu85@gmail.com>
2018-07-28 11:09:19 -0700

    create score get, post, delete controllers. revert models back to es5 =*(. update question model to include game _id.

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 // Exporting an object containing all of our models
 
-export * from "./Admin";
-export * from "./Game";
-export * from "./Score";
-export * from "./User";
-export * from "./Question";
+module.exports = {
+    Admin: require("./Admin"),
+    Game: require("./Game"),
+    Question: require("./Question"),
+    Score: require("./Score"),
+    User: require("./User")
+};
