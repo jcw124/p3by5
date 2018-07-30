57d8b0c27e053c07b851f16d074daece490d4f4f
Janell Williams <jcw_124@sbcglobal.net>
2018-07-29 21:06:04 -0700

    render questions

--- client/src/components/Questions/Questions.js
+++ client/src/components/Questions/Questions.js
@@ -1,16 +1,63 @@
-import React from "react";
-import "./Questions.css"; 
+//import React from "react";
+import React, { Component } from "react";
+import "./Questions.css";
+import data from "./Questions.json";
 
 
-const Questions = props => (
-    <div>
-      <div className="questions">
-        This is the Questions Componenet
-      </div>
-    </div>
-  );
+class Questions extends Component {
+  state = {
+    data: [{}]
+  };
+
   
-  export default Questions;
+componentDidMount() {
+   this.setState({ data: data });
+ }
+
+  handleclick = (answerid, questionid) => {
+    if (
+      data[questionid].correctAnswer ===
+      data[questionid].possibleAnswers[answerid]
+    ) {
+     console.log("You are Correct");
+    } else {
+      console.log("You are Incorrect");
+    }
+  };
+
+  render() {
+    return (
+      <div> 
+        <div className="questions">
+          This is the Questions Componenet
+          {this.state.data.map(item => (
+            
+      <div>
+              <div>{item.question}</div>
+              <ul>
+                <li onclick={this.handleclick(0, 0)}>
+                {item.possibleAnswers[0]}
+                </li>
+                {/* <li onclick={this.handleclick(1, 0)}>
+                  {item.possibleAnswers[1]}
+                </li>
+                <li onclick={this.handleclick(2, 0)}>
+                  {item.possibleAnswers[2]}
+                </li>
+                <li onclick={this.handleclick(3, 0)}>
+                  {item.possibleAnswers[3]} */} 
+                {/* </li> */}
+              </ul>
+            </div>
+    )
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Questions;
 
 
   //pull 10 questions and answers from the quiz db 
