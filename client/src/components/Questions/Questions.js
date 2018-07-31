364abab6a30084fbbef367061c26a9bf28001775
Janell Williams <jcw_124@sbcglobal.net>
2018-07-30 17:27:01 -0700

    commiting to merge

--- client/src/components/Questions/Questions.js
+++ client/src/components/Questions/Questions.js
@@ -28,31 +28,35 @@ componentDidMount() {
   render() {
     return (
       <div> 
-        <div className="questions">
+        <div className="questions"></div>
           This is the Questions Componenet
           {this.state.data.map(item => (
             
-      <div>
-              <div>{item.question}</div>
+ 
+              <div className="questiondisplay">{item.question}</div>
+              
+          <div>    {this.state.date.possibleAnswers.map(( item, i) => {
+  return (
               <ul>
                 <li onclick={this.handleclick(0, 0)}>
                 {item.possibleAnswers[0]}
                 </li>
-                {/* <li onclick={this.handleclick(1, 0)}>
+               <li onclick={this.handleclick(1, 0)}>
                   {item.possibleAnswers[1]}
                 </li>
                 <li onclick={this.handleclick(2, 0)}>
                   {item.possibleAnswers[2]}
                 </li>
                 <li onclick={this.handleclick(3, 0)}>
-                  {item.possibleAnswers[3]} */} 
-                {/* </li> */}
+                  {item.possibleAnswers[3]} 
+               </li> 
               </ul>
-            </div>
-    )
-          )}
-        </div>
-      </div>
+  )}    
+</div>
+)}    
+    )}
+  
+    </div> 
     );
   }
 }
@@ -65,3 +69,19 @@ export default Questions;
   // randomize questions selected if more than the number required exist in the db. 
 
   //this.question, this.choices, this.answer
+
+
+  // list.map((item, index) => {
+  //   return (
+  //     <div key={index}>
+  //       <ul >{item.value}</ul>
+  //      {
+  //       item.list.map((subitem, i) => {
+  //         return (
+  //            <ul ><li>{subitem.value}</li></ul>
+  //         )
+  //       })
+  //      }
+  //     </div>
+  //   )
+  // }
